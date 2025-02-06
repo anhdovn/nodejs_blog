71d1532af88f2f61be48c887dd0b46bc300a9d4d
anhddabilive <anhdd@abilive.vn>
2025-02-06 16:57:24 +0700

    template engine

--- src/app.js
+++ src/app.js
@@ -1,22 +1,28 @@
-import express from 'express';
-import morgan from 'morgan';
-import { engine } from 'express-handlebars';
+import express from 'express'; // (express is a library.)
+import morgan from 'morgan'; // tool logger is a library.
+import { engine } from 'express-handlebars'; // template engine for writing HTML easily. ( express-handlebars is a library.)
 
+// define __dirname ( current URL )
+import path from 'path'; // from nodejs ( not a library )
+import { fileURLToPath } from 'url'; // from nodejs ( not a library )
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// define express app
 const app = express();
 const port = 3000;
 
-// Template engine
+// Use template engine
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './asset/views');
+app.set('views', path.join(__dirname, 'assets/views'));
 
-// Morgan - Tool logger
+// Use Morgan - Tool logger
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World2!');
+  res.render('home');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`localhost:${port}`);
 });
